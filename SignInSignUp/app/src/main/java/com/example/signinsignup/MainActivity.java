2a82c7d740cf072cd3b6ee0864452a83573901a7
Praveen Ukkoji <praveenukkoji@gmail.com>
2021-08-02 19:37:16 +0530

    app 3

--- SignInSignUp/app/src/main/java/com/example/signinsignup/MainActivity.java
+++ SignInSignUp/app/src/main/java/com/example/signinsignup/MainActivity.java
@@ -3,122 +3,69 @@ package com.example.signinsignup;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean isValid = false;
+    EditText username,password;
+    Button signUpBtn;
 
-    public Crendentials crendentials;
-
-    SharedPreferences sharedPreferences;
-    SharedPreferences.Editor sharedPreferencesEditor;
-
-    int attempt = 5;
+    String regularExpr="^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!])[A-Za-z\\d@$!]{8,}$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText email;
-        EditText password;
-
-        CheckBox rememberme;
-
-        TextView register;
-
-        Button signin;
-
-        email = findViewById(R.id.email);
+        username = findViewById(R.id.username);
         password = findViewById(R.id.password);
+        signUpBtn = findViewById(R.id.signup);
 
-        rememberme = findViewById(R.id.rememberme);
-
-        register = findViewById(R.id.register);
-
-        signin = findViewById(R.id.signup);
-
-        crendentials = new Crendentials();
-
-        sharedPreferences = getApplicationContext().getSharedPreferences("CrendentialsDB", MODE_PRIVATE);
-        sharedPreferencesEditor = sharedPreferences.edit();
-
-        if(sharedPreferences != null){
-
-            Map<String, ?> preferencesMap = sharedPreferences.getAll();
-
-            if(preferencesMap.size() != 0){
-                crendentials.loadCrendentials(preferencesMap);
-            }
-
-            String savedEmail = sharedPreferences.getString("LastSavedEmail", "");
-            String savedPassword = sharedPreferences.getString("LastSavedPassword", "");
-
-            if(sharedPreferences.getBoolean("RememberMe", false)){
-                email.setText(savedEmail);
-                password.setText(savedPassword);
-                rememberme.setChecked(true);
-            }
-        }
-
-        rememberme.setOnClickListener(new View.OnClickListener() {
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String uname = username.getText().toString();
+                String pwd = password.getText().toString();
 
-                sharedPreferencesEditor.putBoolean("RememberMe", rememberme.isChecked());
-                sharedPreferencesEditor.apply();
-            }
-        });
+                if(validatePassword(pwd)){
 
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
-                startActivity(intent);
-            }
-        });
+                    Bundle bundle = new Bundle();
 
-        signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+                    bundle.putString("username",uname);
+                    bundle.putString("password",pwd);
 
-                String input_email = email.getText().toString();
-                String input_password = password.getText().toString();
-                
-                if(input_email.isEmpty() || input_password.isEmpty()){
-                    Toast.makeText(MainActivity.this, "Please enter all crendentials.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this,SignIn.class);
+
+                    intent.putExtras(bundle);
+
+                    startActivity(intent);
+
+
                 }
                 else{
-                    isValid = validate(input_email, input_password);
-
-                    if(isValid && attempt > 0){
-                        sharedPreferencesEditor.apply();
-
-                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                        startActivity(intent);
-                    }
-                    else{
-//                        Toast.makeText(MainActivity.this, "Incorrect crendentials.", Toast.LENGTH_SHORT).show();
-                        if (attempt > 0)
-                            attempt = attempt - 1;
-                        Toast.makeText(MainActivity.this, "SignIn Attempts: " + attempt + " left.", Toast.LENGTH_SHORT).show();
-                    }
+                    Toast.makeText(MainActivity.this, "Invaild Password", Toast.LENGTH_SHORT).show();
                 }
+
             }
         });
+
+
+
     }
 
-    boolean validate(String email, String password){
-        return crendentials.verifyCrendentials(email, password);
+    public boolean validatePassword(String pwd){
+        Pattern pattern = Pattern.compile(regularExpr);
+        Matcher matcher = pattern.matcher(pwd);
+
+        return matcher.matches();
+
+
     }
 }
